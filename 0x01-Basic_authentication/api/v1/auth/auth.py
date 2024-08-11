81ae1ab0bc6e7c2fc5c61223065964e8ba148127
EMarvelM <marvelousegbe19@gmail.com>
2024-08-11 06:32:11 +0100

    templete of class to manage the API authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,16 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    def __init__(self) -> None:
+        pass
+
+    def require_auth(self, path: str, exclude_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
