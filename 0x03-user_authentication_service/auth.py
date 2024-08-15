b7101cf109b4e3a07079f025e1958f286b262fe4
EMarvelM <marvelousegbe19@gmail.com>
2024-08-15 21:03:47 +0100

    line longer than 80

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,7 +29,7 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
-            return self._db.add_user(email=email, hashed_password=hashed_password)
+            return self._db.add_user(email, hashed_password)
 
 
 def _hash_password(password: str) -> bytes:
