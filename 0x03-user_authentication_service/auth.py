19d4a686a71014e2a9240103f6a89eda5353ee8a
EMarvelM <marvelousegbe19@gmail.com>
2024-08-15 20:49:28 +0100

    return the user object

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,34 @@
 """ Auth module
 """
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a new user
+
+        Args:
+            email (str): email
+            password (str): password
+
+        Returns:
+            User: User object
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email=email, hashed_password=hashed_password)
 
 
 def _hash_password(password: str) -> bytes:
