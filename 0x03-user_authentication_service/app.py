8b571058af5ebf0337c651e3f0cb00e86226cb8f
EMarvelM <marvelousegbe19@gmail.com>
2024-08-15 21:31:31 +0100

    In this task, you will set up a basic Flask app.
    Create a Flask app that has a single GET route ("/") and use flask.jsonify to return a JSON payload of the form

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
