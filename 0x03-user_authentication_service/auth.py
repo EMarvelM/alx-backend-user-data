1c6878d2fef60aed7b0e85b745e2695cced4e9fc
EMarvelM <marvelousegbe19@gmail.com>
2024-08-16 15:49:59 +0100

    set _hash_password straight

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -116,9 +116,8 @@ class Auth:
             raise ValueError
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = _hash_password(password)
             self._db.update_user(
-                user.id, hashed_password=hashed_password, reset_token=None
+                user.id, hashed_password=_hash_password(password), reset_token=None
             )
         except (NoResultFound, InvalidRequestError):
             raise ValueError
