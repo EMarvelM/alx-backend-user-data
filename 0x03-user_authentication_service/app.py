5487a35f2362702e9ce85ce0f653f816f5cd6c7f
EMarvelM <marvelousegbe19@gmail.com>
2024-08-16 14:51:34 +0100

    In this task, you will implement a profile function to respond to the GET /profile route.
    The request is expected to contain a session_id cookie. Use it to find the user. If the user exist, respond with a 200 HTTP status and the following JSON payload:
    
    {"email": "<user email>"}
    
    If the session ID is invalid or the user does not exist, respond with a 403 HTTP status.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,19 @@ def logout():
         return abort(403)
 
 
+@app.route("/profile", strict_slashes=False)
+def profile():
+    """ Route to get the user profile
+    """
+    session_id = request.cookies.get("session_id")
+
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if not user:
+            return abort(403)
+        return jsonify({"email": user.email})
+    else:
+        return abort(403)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
