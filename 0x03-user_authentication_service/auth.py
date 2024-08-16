e4022ed94a49366cf13fdd2539fff5bc80b79b2b
EMarvelM <marvelousegbe19@gmail.com>
2024-08-16 15:54:18 +0100

    trial

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,28 +103,16 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """ Update the password
-
-        Args:
-            reset_token (str): reset token
-            password (str): password
-
-        Returns:
-            None
         """
-        if not reset_token or not password:
-            raise ValueError
+        update_password.
+        """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            self._db.update_user(
-                user.id, hashed_password=_hash_password(password),
-                reset_token=None
-            )
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
-        except InvalidRequestError:
-            raise ValueError
-        return None
 
 
 def _generate_uuid() -> str:
